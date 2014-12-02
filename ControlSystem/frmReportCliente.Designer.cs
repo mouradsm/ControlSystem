07d6bf39506d757d2a8008fc54a3ca67ed25bb2c
mouradsm <diegosouza20049@hotmail.com>
2014-12-02 19:42:46 -0200

    atualizações

---
+++ ControlSystem/frmReportCliente.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ControlSystem
+{
+    partial class frmReportCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.enderecoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listView1 = new ControlSystem.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.enderecoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enderecoBindingSource
+            // 
+            this.enderecoBindingSource.DataSource = typeof(ControlSystem.endereco);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.Location = new System.Drawing.Point(12, 136);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(760, 97);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Cód. Cliente";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CPF/CNPF";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Endereço";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Data de Cadastramento";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Status";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Tipo";
+            // 
+            // frmReportCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 358);
+            this.Controls.Add(this.listView1);
+            this.Name = "frmReportCliente";
+            this.Text = "frmReportCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.enderecoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource enderecoBindingSource;
+        private ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
